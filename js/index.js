85ee714a5d1da1fb2a6fecdff427094e29072076
sishir123 <87405061+sishir123@users.noreply.github.com>
2021-09-04 18:44:46 +0545

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 const slides = document.querySelectorAll(".slide");
 const next = document.querySelector("#next");
 const prev = document.querySelector("#prev");
+const time = document.getElementById("time");
+const day = document.getElementById("day");
 const intervalTime = 3000;
 let slideInterval;
 let counter = 0;
@@ -29,6 +31,19 @@ const prevSlide = () => {
 	}
 	setTimeout(() => current.classList.remove("current"));
 };
+setDate = () => {
+	let hours = new Date().getHours();
+	let minutes = new Date().getMinutes();
+	let seconds = new Date().getSeconds();
+	const currentDay = new Date().toDateString();
+	hours = hours >= 10 ? hours : `0${hours}`;
+	minutes = minutes >= 10 ? minutes : `0${minutes}`;
+	seconds = seconds >= 10 ? seconds : `0${seconds}`;
+	time.innerHTML = `${hours} : ${minutes} : ${seconds}`;
+	day.innerHTML = currentDay;
+};
+setDate();
+setInterval(setDate, 1000);
 
 next.addEventListener("click", (e) => {
 	nextSlide();
